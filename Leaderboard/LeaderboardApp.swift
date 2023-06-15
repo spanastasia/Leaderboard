b17f781e4899cd0f98ce7e6ff56ef9d716ae6c07
Anastasiia Spiridonova <anastasiia_spiridonova@epam.com>
2023-06-15 11:44:10 +0300

    Initial Commit

---
+++ Leaderboard/LeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaderboardApp.swift
+//  Leaderboard
+//
+//  Created by Anastasiia Spiridonova on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct LeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
